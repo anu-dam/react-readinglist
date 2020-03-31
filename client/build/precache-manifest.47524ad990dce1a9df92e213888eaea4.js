38c944a994c0251811be7ee4b9f97b52cc707395
Anuradha Dambaladeniya <anuradhadambaladeniya@Anuradhas-MBP.modem>
2020-03-31 15:00:26 +1030

    build

---
+++ client/build/precache-manifest.47524ad990dce1a9df92e213888eaea4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca05f219f138981a54e6",
+    "url": "/static/css/main.c603a93b.chunk.css"
+  },
+  {
+    "revision": "ca05f219f138981a54e6",
+    "url": "/static/js/main.09fb4462.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cb17bcf018bf7c55997e",
+    "url": "/static/js/2.09d13a7d.chunk.js"
+  },
+  {
+    "revision": "0419c80fb0a57d56005b6be5c897dbbe",
+    "url": "/index.html"
+  }
+];
